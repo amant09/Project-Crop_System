63c3cf33edf5b94d6aa98e9deaafbb0e79f7446a
Tiwari <aman.tiwari@capgemini.com>
2021-07-06 23:08:02 +0530

    minute changes

--- Dealers/controllers/dealerControl.js
+++ Dealers/controllers/dealerControl.js
@@ -1,6 +1,7 @@
-//const axios = require('axios');
+const axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
 const {Crop} = require('../models/db')
+
 
 module.exports.searchCrop = (req , res )=> {
     
@@ -13,64 +14,38 @@ module.exports.searchCrop = (req , res )=> {
           const coll = client.db('ProjectFarmer').collection('crops');
           const name = req.params.name;
           const query = {cropName : name}
-          coll.find(query).toArray(function(err, result) {
+          coll.findOne(query).toArray(function(err, result) {
             if (err) throw err;
             res.send(result);
+            console.log(result.price);
           client.close();
         });
     })
 }
+const url = 'mongodb+srv://aman09:Amant123@aman-practice.k5xrh.mongodb.net/ProjectFarmer?retryWrites=true&w=majority'
 
 module.exports.buyCrop =  (req, res)=>{
   const crop = req.body;
   Crop.create(crop).then((response)=>{ 
-      if(crop.quantity == 325 && crop.cropName == "Wheat"){
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ProjectFarmer");
+        dbo.collection("crops").findOne({cropName: crop.cropName}, function(err, result) {
+          if (err) throw err;
+          const price = result.price;
+          console.log(price)
+          db.close();
+       
+   
           
-          res.status(201).send(`Thankyou for Ordering ${crop.cropName} with us!
-          Your Bill is Rs. ${crop.quantity*18}`);
-      }
-      else if(crop.quantity == 150 && crop.cropName == "Wheat"){
-          
-          res.status(201).send(`Thankyou for Ordering ${crop.cropName} with us!
-          Your Bill is Rs. ${crop.quantity*18}`);
-      }
-      else if(crop.quantity >= 500 && crop.cropName == "Wheat"){
-          res.send(`OOPS! We are out of Stock!
+      res.status(201).send(`Thankyou for Ordering ${crop.cropName} with us!
 
-          Inconveniece is deeply regreted`)
-      }
-      if(crop.quantity == 7 && crop.cropName == "Peas"){
-          
-          res.status(201).send(`Thankyou for Ordering ${crop.cropName} with us!
+Item ID: ${response._id}
 
-          Your Bill is Rs. ${crop.quantity*20}`);
-      }
-      else if(crop.quantity == 50 && crop.cropName == "Mango"){
-          
-          res.status(201).send(`Thankyou for Ordering ${crop.cropName} with us!
+Your Bill is Rs. ${crop.quantity*price}`); 
+});
+    });
 
-          Your Bill is Rs. ${crop.quantity*40}`);
-      }
-      else if(crop.quantity == 20 && crop.cropName == "Potato"){
-          
-          res.status(201).send(`Thankyou for Ordering ${crop.cropName} with us!
-
-          Your Bill is Rs. ${crop.quantity*16}`);
-      }
-      else if(crop.quantity == 15 && crop.cropName == "Chana Dal"){
-          
-          res.status(201).send(`Thankyou for Ordering ${crop.cropName} with us!
-
-          Your Bill is Rs. ${crop.quantity*85}`);
-      }
-      else if(crop.quantity >= 70 && crop.cropName == "Mango"){
-          res.send(`OOPS! We are out of Stock!
-          Inconveniece is deeply regreted`)
-      }
-      else{
-          res.send("You have not entered correct data!")
-      }
-      
 
   }).catch((err)=>{
       if(err){
